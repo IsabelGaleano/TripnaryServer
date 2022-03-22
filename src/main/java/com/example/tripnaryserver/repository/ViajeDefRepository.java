aa1c0cc06501656907dbac20de535b692703d0f5
Isabel <isagaleano9@gmail.com>
2022-03-22 13:09:13 -0600

    Add filter method

--- src/main/java/com/example/tripnaryserver/repository/ViajeDefRepository.java
+++ src/main/java/com/example/tripnaryserver/repository/ViajeDefRepository.java
@@ -6,7 +6,10 @@ import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @EnableScan
 @Repository
 public interface ViajeDefRepository extends CrudRepository<ViajeDef, String> {
+    public List<ViajeDef> findViajeDefByIdUsuario(String idUsuario);
 }
